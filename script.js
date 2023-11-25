b788b48e050052414822e4aa522dbbba1e497fdd
Sanskar6767 <sanskar62777sharma@gmail.com>
2023-11-25 13:04:36 +0530

    The calculator is ready

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let a = null;
 let b = null;
 let operator = null;
 let result = 0;
+let isError = false; 
 
 let textArea = document.querySelector('.text-area');
 let buttonGrid = document.querySelector('.button-grid');
@@ -13,18 +14,42 @@ buttonGrid.addEventListener('click', function (e){
         const buttonText = e.target.textContent;
 
         if (e.target.classList.contains('num')){
+            if (!checkDisplayLength(buttonText)) return;
             if (operator === null) {
                 a = a === null ? buttonText : a + buttonText;
                 textArea.textContent = `${a}`;
             } else {
                 b = b === null ? buttonText : b + buttonText;
-                textArea.textContent += `${b}`;
+                textArea.textContent = `${a}${operator}${b}`;
             }
         } else if (e.target.classList.contains('op')) {
-            operator = buttonText;
-            textArea.textContent += `${operator}`;
+            if (!checkDisplayLength()) return;
+            if (b === null) {
+                operator = buttonText;
+                textArea.textContent = `${a}${operator}`;
+            } else if (b !== null && operator !== null) {
+                operate(a,b,operator);
+                if (isError) {
+                    isError = false;
+                    return;
+                }
+                a = result;
+                operator = buttonText;
+                textArea.textContent = `${a}${operator}`;
+                b = null;
+            }
+            
         } else if (e.target.classList.contains('clear')) {
-            console.log("its: ", buttonText);
+            if (b !== null && b.length > 0) {
+                b = b.slice(0, -1);
+                textArea.textContent = `${a}${operator}${b}`;
+            } else if (operator !== null) {
+                operator = null;
+                textArea.textContent = `${a}`;
+            } else if (a !== null && a.length > 0) {
+                a= a.slice(0, -1);
+                textArea.textContent = `${a}`;
+            }
         } else if (e.target.classList.contains('AC')) {
             a = null;
             b = null;
@@ -33,10 +58,26 @@ buttonGrid.addEventListener('click', function (e){
             textArea.textContent = `${result}`;
         } else if (e.target.classList.contains('equal')) {
             operate(a,b,operator);
+            if (isError) {
+                isError = false;
+                return;
+            }
             a = result;
             textArea.textContent = `${a}`;
             operator = null;
             b = null;
+        }else if (e.target.classList.contains('decimal')){
+            if (operator === null) {
+                if (!a || !a.includes('.')){
+                    a = a === null ? '0' + buttonText : a + buttonText;
+                }
+                textArea.textContent = `${a}`;
+            } else {
+                if (!b || !b.includes('.')){
+                    b = b === null ? '0' + buttonText : b + buttonText;
+                }
+                textArea.textContent = `${a}${operator}${b}`;
+            } 
         }
     }
 });
@@ -44,38 +85,115 @@ buttonGrid.addEventListener('click', function (e){
 function operate(a,b,operator){
     if (a != null && b != null && operator != null){
         if (operator === "+") {
-            sum(a,b);
+            sum(a, b);
         } else if (operator === "-") {
-            subtract(a,b);
+            subtract(a, b);
         } else if (operator === "*") {
-            multiply(a,b);
+            multiply(a, b);
         } else if (operator === "/") {
-            divide(a,b);
+            divide(a, b);
+            if (parseFloat(b) === 0) return;
+        }
+        if (isError) {
+            return;
         }
         textArea.textContent = `${result}`;
     }
 };
 
+function checkDisplayLength(proposedInput) {
+    let displayContent = a || '';
+    if (operator) displayContent += operator;
+    if (b || proposedInput) displayContent += b || proposedInput;
+    return displayContent.length <= 14;
+}
 
-// Mathematical functions 
-function sum(a,b) {
-    return result = parseInt(a) + parseInt(b);
-}
-function subtract(a,b) {
-    return result = parseInt(a) - parseInt(b);
-}
-function multiply(a,b) {
-    return result = parseInt(a) * parseInt(b);
-}
-function divide(a,b) {
-    if (parseInt(b) === 0) {
-        alert("I am not falling for that");
-    } else {
-    return result = parseInt(a) / parseInt(b);
+// Keyboard Functionality (Bonus) 
+document.addEventListener('keydown', function(event) {
+    event.preventDefault();
+    let key = event.key;
+
+    switch (key) {
+        case '0':
+        case '1':
+        case '2':
+        case '3':
+        case '4':
+        case '5':
+        case '6':
+        case '7':
+        case '8':
+        case '9':
+            simulateButtonClick(`num-${key}`);
+            break;
+        case '+':
+            simulateButtonClick(`plus`);
+            break;
+        case '-':
+            simulateButtonClick(`minus`);
+            break;
+        case '*':
+            simulateButtonClick(`multiply`);
+            break;
+        case '/':
+            simulateButtonClick(`divide`);
+            break;
+        case 'Enter':
+            simulateButtonClick('equal');
+            break;
+        case 'Backspace':
+            simulateButtonClick('clear');
+            break;
+        case '.':
+            simulateButtonClick('decimal');
+            break;
+    }
+});
+
+function simulateButtonClick(buttonClass) {
+    let button = document.querySelector(`.${buttonClass}`);
+    if (button) {
+        button.click(); 
     }
 }
 
-// pasting in textArea 
+
+// Mathematical functions 
+function sum(a,b) {
+    result = parseFloat(a) + parseFloat(b);
+    if (result.toString().includes('.')){
+        return result = result.toFixed(2);
+    }
+    return result;
+}
+function subtract(a,b) {
+    result = parseFloat(a) - parseFloat(b);
+    if (result.toString().includes('.')){
+        return result = result.toFixed(2);
+    }
+    return result;
+}
+function multiply(a,b) {
+    result = parseFloat(a) * parseFloat(b);
+    if (result.toString().includes('.')){
+        return result = result.toFixed(2);
+    }
+    return result;
+}
+function divide(a,b) {
+    if (parseFloat(b) === 0) {
+        textArea.textContent = `Bond, James Bond!`;
+        isError = true;
+        return ;
+    } else {
+    result = parseFloat(a) / parseFloat(b);
+    if (result.toString().includes('.')){
+        return result = result.toFixed(2);
+    }
+    return result;
+    }
+}
+
 
 
 
